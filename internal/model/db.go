17d885cd92b13bb34d0fef8be80d05a1f3989d75
azuki774 <azuki774s@gmail.com>
2022-12-08 01:05:25 +0900

    fix gorm

--- internal/model/db.go
+++ internal/model/db.go
@@ -7,9 +7,9 @@ func (ElectConsumption) TableName() string {
 }
 
 type ElectConsumption struct {
-	ID               int64     `gorm:"id,primaryKey"`
-	RecordDate       string    `gorm:"column:record_date,index,not null,unique"` // YYYYMMDD
-	TotalConsumption int64     `gorm:"column:total_consumption,not null"`
+	ID               int64     `gorm:"column:id"`
+	RecordDate       string    `gorm:"column:record_date"` // YYYYMMDD
+	TotalConsumption int64     `gorm:"column:total_consumption"`
 	DayConsumption   int64     `gorm:"column:day_consumption"`
 	NightConsumption int64     `gorm:"column:night_consumption"`
 	CreatedAt        time.Time `gorm:"column:created_at"`
