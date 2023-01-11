033f6d27740c2effed692668490c282f1342a47e
azuki774 <azuki774s@gmail.com>
2023-01-11 22:41:33 +0900

    fixup! wip API

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module azuki774/bill-manager
 go 1.18
 
 require (
+	github.com/go-chi/chi v1.5.4
 	github.com/goark/koyomi v0.6.4
 	github.com/jarcoal/httpmock v1.2.0
 	github.com/spf13/cobra v1.4.0
--- internal/server/server.go
+++ internal/server/server.go
@@ -4,7 +4,11 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
+	"github.com/go-chi/chi"
 	"go.uber.org/zap"
 )
 
@@ -15,16 +19,47 @@ type Server struct {
 	APISvc APIService
 }
 
+func addRouting(r *chi.Mux) {
+	r.Route("/", func(r chi.Router) {
+		r.Get("/", func(w http.ResponseWriter, r *http.Request) { // GET /
+			w.Write([]byte("OK"))
+		})
+	})
+}
+
 func (s *Server) Start(ctx context.Context) (err error) {
 	s.Logger.Info("server start")
-	addr := fmt.Sprintf(":%s", s.Port)
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("OK"))
-	})
 
-	if err := http.ListenAndServe(addr, nil); err != nil {
+	addr := fmt.Sprintf(":%s", s.Port)
+
+	r := chi.NewRouter()
+	addRouting(r)
+
+	server := &http.Server{
+		Addr:    addr,
+		Handler: r,
+	}
+
+	ctxIn, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+	defer stop()
+
+	var errCh = make(chan error)
+	go func() {
+		errCh <- server.ListenAndServe()
+	}()
+
+	<-ctxIn.Done()
+	if nerr := server.Shutdown(ctx); nerr != nil {
+		s.Logger.Error("failed to shutdown server", zap.Error(nerr))
+		return nerr
+	}
+
+	err = <-errCh
+	if err != nil && err != http.ErrServerClosed {
+		s.Logger.Error("failed to close server", zap.Error(err))
 		return err
 	}
 
+	s.Logger.Info("http server close gracefully")
 	return nil
 }
